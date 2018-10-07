b2452181cfffb85d2ff2f046d30f7084fd17ce19
Theavid <x934198@gmail.com>
2018-10-08 02:56:43 +0800

    search

--- js/search.js
+++ js/search.js
@@ -1,18 +1,27 @@
 //http://tarruda.github.io/bootstrap-datetimepicker/
 
-var onBtnQueryPopular = function (e){      
-  $.ajax({
-    type: "GET",
-    url: "https://staging.threal3d.com/api/v3/search/tags/popular?number_of_stories=6",
-    //contentType: 'application/json; charset=UTF-8', 
-    success: function(data, status, jqXHR) {
+// 
+var btn_query_popular = function (e)
+{      
+   var x = document.getElementById("myText").value;
+        console.log(x);
+  $.ajax
+  (
+    {
+      type: "GET",
+      url: "https://staging.threal3d.com/api/v3/search?keyword="+x,
+      //contentType: 'application/json; charset=UTF-8', 
+
+      success: function(data, status, jqXHR) 
+      {
         var str = "";
-        for (var i = 0; i < data[0]["stories"].length; i++) {
+        for (var i = 0; i < data["story_description"].length; i++) 
+        {
           if(i % 5 == 0)
             str += "<tr>";
 
           str += "<td>";
-          str += "<img src=\""+ data[0]["stories"][i]["color"]["fit_160"] +"\" width = 150 height = 150>"
+          str += "<img src=\""+ data["story_description"][i]["color"]["fit_160"] +"\" width = 150 height = 150>"
           //str += "<img src=\""+ data[i]["depth"]["original"] +"\">"
           str += "</td>";
           
@@ -22,14 +31,22 @@ var onBtnQueryPopular = function (e){
 
         $("#table_query_result").append(str);
         console.log(data[i]);
-        //alert(data);
+        //alert("null");
       },
-    error: function(jqXHR, textStatus, errorThrown) { 
+
+      error: function(jqXHR, textStatus, errorThrown) 
+      { 
         console.log(errorThrown) ;//alert('Failed!'); 
+        alert("null");
       }
-    });
+    }
+  );
 }
 
-$(document).ready(function(){ 
-    $("#btn_query_popular").on("click", onBtnQueryPopular);
-});
+$(document).ready
+(
+  function()
+  { 
+    $("#btn_query_popular").on("click", btn_query_popular);
+  }
+);
