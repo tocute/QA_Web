89fb79aaa1fc8a6855f92a1271f9b496e709a3e2
Theavid <x934198@gmail.com>
2018-10-24 18:26:31 +0800

    hyperlink 1024

--- js/search_tag.js
+++ js/search_tag.js
@@ -14,7 +14,7 @@ var btn_query_popular = function (e)
       success: function(data, status, jqXHR) 
       {
         var str = "";
-
+        var jump_url = "story_info.html?story_id=";
         for (var i = 0 ; i < data["story_tag"].length ; i++) 
         {
           if(i % 5 == 0)
@@ -32,14 +32,17 @@ var btn_query_popular = function (e)
             str += "</tr>";
         }
 
-
+/////////////////
         for (var i = 0 ; i < data["story_description"].length ; i++) 
         {
           if(i % 5 == 0)
             str += "<tr>";
-
+           var jump_url_with_id = "story_info.html?story_id=" + data["story_description"][i]["id"];
           str += "<td>";
-          str += "<img src=\""+ data["story_description"][i]["color"]["fit_160"] +"\" width = 150 height = 150>"
+           str += "<a href=\"" + jump_url_with_id + " \">";     
+          str += "<img src=\" " + data["story_description"][i]["color"]["fit_160"] + " \" onmouseover=\"src=' " + data["story_description"][i]["depth"]["original"] + "'\" onmouseout=\"src='" + data["story_description"][i]["color"]["fit_160"] + "'\" width=160 max-height=160>";
+           str += "</a>";          
+          // str += "<img src=\""+ data["story_description"][i]["color"]["fit_160"] +"\" width = 150 height = 150>"
           //str += "<img src=\""+ data[i]["depth"]["original"] +"\">"
           str += "</td>";
           
